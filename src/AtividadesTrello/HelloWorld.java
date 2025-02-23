df20d4875cf221f411c05d1792fcedc6a1c94d31
LucasSarmento1 <manu.asn@gmail.com>
2025-02-23 16:28:54 -0300

    Primeiro commit

---
+++ src/AtividadesTrello/HelloWorld.java
@@ -0,0 +1,10 @@
+package AtividadesTrello;
+
+public class HelloWorld {
+    public static void main(String[] ages) {
+
+        //Missão do progrador de Sucesso. Seu primeiro passo.
+        System.out.println("Hello World!!!");
+
+    }
+}
